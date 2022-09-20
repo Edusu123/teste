320706d745a1f6bf33621e0c73e21da2cfb5b97c
Eduardo Conde Pires <eduardo.c.pires@outlook.com>
2022-09-20 18:17:08 -0300

    Adicionando métodos (calculadora) na controller/removendo código não utilizado

---
+++ testeAPI/Controllers/CalculadoraController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testeAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CalculadoraController : Controller
+    {
+        [HttpPost]
+        [Route("soma")]
+        public decimal Soma(Numeros nums)
+        {
+            return nums.Num1 + nums.Num2;
+        }
+
+        [HttpPost]
+        [Route("subtracao")]
+        public decimal Subtracao(Numeros nums)
+        {
+            return nums.Num1 - nums.Num2;
+        }
+
+        [HttpPost]
+        [Route("multiplicacao")]
+        public decimal Multiplicacao(Numeros nums)
+        {
+            return nums.Num1 * nums.Num2;
+        }
+
+        [HttpPost]
+        [Route("divisao")]
+        public decimal Divisao(Numeros nums)
+        {
+            if(nums.Num2 == 0)
+            {
+                return 0;
+            }
+
+            return nums.Num1 / nums.Num2;
+        }
+    }
+}
---
+++ testeAPI/Numeros.cs
@@ -0,0 +1,9 @@
+﻿namespace testeAPI
+{
+    public class Numeros
+    {
+        public decimal Num1 { get; set; }
+
+        public decimal Num2 { get; set; }
+    }
+}
